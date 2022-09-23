b90d7f7a13a4c732b2cb8bfc45f79f95a023a10e
Ivan Petrukhin <monkpatch@gmail.com>
2022-09-23 19:03:00 +0300

    Drunken man

--- Class3/Task1/Task1.cs
+++ Class3/Task1/Task1.cs
@@ -1,4 +1,6 @@
 ﻿// Колода
+
+using System.ComponentModel.DataAnnotations.Schema;
 using Deck = System.Collections.Generic.List<Card>;
 // Набор карт у игрока
 using Hand = System.Collections.Generic.List<Card>;
@@ -8,67 +10,179 @@ using Table = System.Collections.Generic.List<Card>;
 // Масть
 internal enum Suit
 {
+    Diamonds,
+    Clubs,
+    Hearts,
+    Spades
 }
 
 // Значение
 internal enum Rank
 {
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    Ace
 }
 
 // Карта
-record Card;
+record Card(Suit Suit, Rank Rank)
+{
+    public override string ToString()
+    {
+        return $"{this.Suit} {this.Rank}";
+    }
+}
 
 // Тип для обозначения игрока (первый, второй)
 internal enum Player
 {
+    First,
+    Second
 }
+
+internal class NoCardsException : Exception
+{
+    public Table Table { get; init; }
+
+    public NoCardsException(Table table)
+    {
+        Table = table;
+    }
+}
+
 
 namespace Task1
 {
     public class Task1
     {
-
- /*
- * Реализуйте игру "Пьяница" (в простейшем варианте, на колоде в 36 карт)
- * https://ru.wikipedia.org/wiki/%D0%9F%D1%8C%D1%8F%D0%BD%D0%B8%D1%86%D0%B0_(%D0%BA%D0%B0%D1%80%D1%82%D0%BE%D1%87%D0%BD%D0%B0%D1%8F_%D0%B8%D0%B3%D1%80%D0%B0)
- * Рука — это набор карт игрока. Карты выкладываются на стол из начала "рук" и сравниваются
- * только по значениям (масть игнорируется). При равных значениях сравниваются следующие карты.
- * Набор карт со стола перекладывается в конец руки победителя. Шестерка туза не бьёт.
- *
- * Реализация должна сопровождаться тестами.
- */
+        /*
+        * Реализуйте игру "Пьяница" (в простейшем варианте, на колоде в 36 карт)
+        * https://ru.wikipedia.org/wiki/%D0%9F%D1%8C%D1%8F%D0%BD%D0%B8%D1%86%D0%B0_(%D0%BA%D0%B0%D1%80%D1%82%D0%BE%D1%87%D0%BD%D0%B0%D1%8F_%D0%B8%D0%B3%D1%80%D0%B0)
+        * Рука — это набор карт игрока. Карты выкладываются на стол из начала "рук" и сравниваются
+        * только по значениям (масть игнорируется). При равных значениях сравниваются следующие карты.
+        * Набор карт со стола перекладывается в конец руки победителя. Шестерка туза не бьёт.
+        *
+        * Реализация должна сопровождаться тестами.
+        */
 
         // Размер колоды
         internal const int DeckSize = 36;
+        private static readonly Random Rng = new Random();
 
         // Возвращается null, если значения карт совпадают
         internal static Player? RoundWinner(Card card1, Card card2)
         {
-            throw new NotImplementedException();
+            if (card1.Rank > card2.Rank)
+                return Player.First;
+            if (card1.Rank < card2.Rank)
+                return Player.Second;
+            return null;
         }
-        
+
 // Возвращает полную колоду (36 карт) в фиксированном порядке
-        internal static Deck FullDeck() 
+        internal static Deck FullDeck()
         {
-            throw new NotImplementedException();
+            var deck = new Deck();
+            foreach (Suit suit in Enum.GetValues(typeof(Suit)))
+            {
+                foreach (Rank rank in Enum.GetValues(typeof(Rank)))
+                {
+                    deck.Add(new Card(suit, rank));
+                }
+            }
+
+            return deck;
         }
 
 // Раздача карт: случайное перемешивание (shuffle) и деление колоды пополам
-        internal static Dictionary<Player, Hand> Deal(Deck deck) 
+        internal static Dictionary<Player, Hand> Deal(Deck deck)
         {
-            throw new NotImplementedException();
+            var shuffledDeck = deck.OrderBy(_ => Rng.Next()).ToList();
+
+            var dict = new Dictionary<Player, Hand>
+            {
+                [Player.First] = shuffledDeck.GetRange(0, shuffledDeck.Count / 2),
+                [Player.Second] = shuffledDeck.GetRange(shuffledDeck.Count / 2 - 1, shuffledDeck.Count / 2)
+            };
+            return dict;
         }
 
 // Один раунд игры (в том числе спор при равных картах).
 // Возвращается победитель раунда и набор карт, выложенных на стол.
-        internal static Tuple<Player, Table> Round(Dictionary<Player, Hand> hands) {
-            throw new NotImplementedException();
+        internal static Tuple<Player, Table> Round(Dictionary<Player, Hand> hands)
+        {
+            if (hands[Player.First].Count == 0 || hands[Player.Second].Count == 0)
+                throw new NoCardsException(new Table());
+
+            var card1 = hands[Player.First][0];
+            var card2 = hands[Player.Second][0];
+            hands[Player.First].RemoveAt(0);
+            hands[Player.Second].RemoveAt(0);
+
+            var table = new Table
+            {
+                card1,
+                card2
+            };
+
+            var winner = RoundWinner(card1, card2);
+            if (winner == null)
+            {
+                try
+                {
+                    var (player, newTable) = Round(hands);
+                    table.AddRange(newTable);
+                    return new Tuple<Player, Table>(player, table);
+                }
+                catch (NoCardsException ex)
+                {
+                    table.AddRange(ex.Table);
+                    throw new NoCardsException(table);
+                }
+            }
+
+            return new Tuple<Player, Table>((Player)winner!, table);
         }
 
 // Полный цикл игры (возвращается победивший игрок)
 // в процессе игры печатаются ходы
-        internal static Player Game(Dictionary<Player, Hand> hands) {
-            throw new NotImplementedException();
+        internal static Player? Game(Dictionary<Player, Hand> hands)
+        {
+            var scores = new Dictionary<Player, int>
+            {
+                [Player.First] = 0,
+                [Player.Second] = 0,
+            };
+            while (true)
+            {
+                try
+                {
+                    var (player, table) = Round(hands);
+                    var score = 0;
+                    scores.TryGetValue(player, out score);
+                    scores[player] = score + table.Count;
+                    Console.WriteLine(
+                        $"Round ended with the victory of player {player:G} ({table[0]:G} vs {table[1]:G})");
+                }
+                catch (NoCardsException)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(
+                $"Score {scores[Player.First]}:{scores[Player.Second]} (player {Player.First} vs player {Player.Second})");
+            if (scores[Player.First] > scores[Player.Second])
+                return Player.First;
+            if (scores[Player.First] < scores[Player.Second])
+                return Player.Second;
+            return null;
         }
 
         public static void Main(string[] args)
@@ -76,7 +190,7 @@ namespace Task1
             var deck = FullDeck();
             var hands = Deal(deck);
             var winner = Game(hands);
-            Console.WriteLine($"Победитель: {winner}");
+            Console.WriteLine($"Winner: player {winner:G}");
         }
     }
 }
--- Class3/Task1/Task1Test.cs
+++ Class3/Task1/Task1Test.cs
@@ -1,6 +1,13 @@
 using NUnit.Framework;
+using NUnit.Framework.Internal;
 using static NUnit.Framework.Assert;
 using static Task1.Task1;
+
+using Deck = System.Collections.Generic.List<Card>;
+// Набор карт у игрока
+using Hand = System.Collections.Generic.List<Card>;
+// Набор карт, выложенных на стол
+using Table = System.Collections.Generic.List<Card>;
 
 namespace Task1;
 
@@ -9,7 +16,9 @@ public class Tests
     [Test]
     public void RoundWinnerTest()
     {
-        throw new NotImplementedException();
+        That(RoundWinner(new Card(Suit.Clubs, Rank.Ace), new Card(Suit.Diamonds, Rank.Ten)), Is.EqualTo(Player.First));
+        That(RoundWinner(new Card(Suit.Spades, Rank.Eight), new Card(Suit.Spades, Rank.Nine)), Is.EqualTo(Player.Second));
+        That(RoundWinner(new Card(Suit.Hearts, Rank.Six), new Card(Suit.Diamonds, Rank.Six)), Is.EqualTo(null));
     }
 
     [Test]
@@ -17,31 +26,175 @@ public class Tests
     {
         var deck = FullDeck();
         That(deck, Has.Count.EqualTo(DeckSize));
-        throw new NotImplementedException();
+        
+        var suits = Enum.GetValues(typeof(Suit));
+        That(suits, Has.Length.EqualTo(4));
+        
+        foreach (Suit suit in suits)
+        {
+            That(deck.Count(card => card.Suit == suit), Is.EqualTo(DeckSize / suits.Length));
+        }
+        
+        var ranks = Enum.GetValues(typeof(Rank));
+        That(ranks, Has.Length.EqualTo(DeckSize / 4));
+        
+        foreach (Rank rank in ranks)
+        {
+            That(deck.Count(card => card.Rank == rank), Is.EqualTo(DeckSize / ranks.Length));
+        }
     }
 
     [Test]
     public void RoundTest()
     {
-        throw new NotImplementedException();
+        var handsFirst1 = new Dictionary<Player, Hand>
+        {
+            [Player.First] = new Hand
+            {
+                new Card(Suit.Hearts, Rank.Ten)
+            },
+            [Player.Second] = new Hand
+            {
+                new Card(Suit.Diamonds, Rank.Eight)
+            }
+        };
+        var handsFirst2 = new Dictionary<Player, Hand>
+        {
+            [Player.First] = new Hand
+            {
+                new Card(Suit.Clubs, Rank.Ace),
+                new Card(Suit.Hearts, Rank.Ten)
+            },
+            [Player.Second] = new Hand
+            {
+                new Card(Suit.Hearts, Rank.Ace),
+                new Card(Suit.Diamonds, Rank.Eight)
+            }
+        };
+        var handsSecond1 = new Dictionary<Player, Hand>
+        {
+            [Player.First] = new Hand
+            {
+                new Card(Suit.Hearts, Rank.Eight)
+            },
+            [Player.Second] = new Hand
+            {
+                new Card(Suit.Diamonds, Rank.Ten)
+            }
+        };
+        var handsSecond2 = new Dictionary<Player, Hand>
+        {
+            [Player.First] = new Hand
+            {
+                new Card(Suit.Clubs, Rank.Ace),
+                new Card(Suit.Hearts, Rank.Eight)
+            },
+            [Player.Second] = new Hand
+            {
+                new Card(Suit.Hearts, Rank.Ace),
+                new Card(Suit.Diamonds, Rank.Ten)
+            }
+        };
+        var handsNone1 = new Dictionary<Player, Hand>
+        {
+            [Player.First] = new Hand
+            {
+                new Card(Suit.Clubs, Rank.Ace),
+            },
+            [Player.Second] = new Hand
+            {
+                new Card(Suit.Hearts, Rank.Ace),
+            }
+        };
+        var handsNone2 = new Dictionary<Player, Hand>
+        {
+            [Player.First] = new Hand
+            {
+                new Card(Suit.Clubs, Rank.Ace),
+                new Card(Suit.Diamonds, Rank.Queen)
+            },
+            [Player.Second] = new Hand
+            {
+                new Card(Suit.Hearts, Rank.Ace),
+                new Card(Suit.Hearts, Rank.Queen)
+            }
+        };
+        var answerFirst1 = new Table
+        {
+            new Card(Suit.Hearts, Rank.Ten),
+            new Card(Suit.Diamonds, Rank.Eight),
+        };
+        var answerFirst2 = new Table
+        {
+            new Card(Suit.Clubs, Rank.Ace),
+            new Card(Suit.Hearts, Rank.Ace),
+            new Card(Suit.Hearts, Rank.Ten),
+            new Card(Suit.Diamonds, Rank.Eight),
+        };
+        var answerSecond1 = new Table
+        {
+            new Card(Suit.Hearts, Rank.Eight),
+            new Card(Suit.Diamonds, Rank.Ten),
+        };
+        var answerSecond2 = new Table
+        {
+            new Card(Suit.Clubs, Rank.Ace),
+            new Card(Suit.Hearts, Rank.Ace),
+            new Card(Suit.Hearts, Rank.Eight),
+            new Card(Suit.Diamonds, Rank.Ten),
+        };
+        var answerNone1 = new Table
+        {
+            new Card(Suit.Clubs, Rank.Ace),
+            new Card(Suit.Hearts, Rank.Ace),
+        };
+        var answerNone2 = new Table
+        {
+            new Card(Suit.Clubs, Rank.Ace),
+            new Card(Suit.Hearts, Rank.Ace),
+            new Card(Suit.Diamonds, Rank.Queen),
+            new Card(Suit.Hearts, Rank.Queen),
+        };
+        That(Round(handsFirst1), Is.EqualTo(new Tuple<Player, Table>(Player.First, answerFirst1)));
+        That(Round(handsFirst2), Is.EqualTo(new Tuple<Player, Table>(Player.First, answerFirst2)));
+        That(Round(handsSecond1), Is.EqualTo(new Tuple<Player, Table>(Player.Second, answerSecond1)));
+        That(Round(handsSecond2), Is.EqualTo(new Tuple<Player, Table>(Player.Second, answerSecond2)));
+        var exNone1 = Throws<NoCardsException>(() => Round(handsNone1))!;
+        That(exNone1.Table, Is.EqualTo(answerNone1));
+        var exNone2 = Throws<NoCardsException>(() => Round(handsNone2))!;
+        That(exNone2.Table, Is.EqualTo(answerNone2));
     }
 
     [Test]
     public void Game2CardsTest()
     {
-        var six = TODO<Card>();
-        var ace = TODO<Card>();
-        Dictionary<Player, List<Card>> hands = new Dictionary<Player, List<Card>>
+        var six = new Card(Suit.Clubs, Rank.Six);
+        var queen1 = new Card(Suit.Spades, Rank.Queen);
+        var queen2 = new Card(Suit.Hearts, Rank.Queen);
+        var ace = new Card(Suit.Diamonds, Rank.Ace);
+        
+        Dictionary<Player, List<Card>> handsFirst = new Dictionary<Player, List<Card>>
         {
-            { TODO<Player>(), new List<Card> {six} },
-            { TODO<Player>(), new List<Card> {ace} }
+            [Player.First] = new List<Card> {ace},
+            [Player.Second] = new List<Card> {six},
         };
-        var gameWinner = Game(hands);
-        That(gameWinner, Is.EqualTo(TODO<Player>()));
-    }
-    
-    private static T TODO<T>()
-    {
-        throw new NotImplementedException();
+        var gameWinnerFirst = Game(handsFirst);
+        That(gameWinnerFirst, Is.EqualTo(Player.First));
+        
+        Dictionary<Player, List<Card>> handsSecond = new Dictionary<Player, List<Card>>
+        {
+            [Player.First] = new List<Card> {queen1, six},
+            [Player.Second] = new List<Card> {queen2, ace}
+        };
+        var gameWinnerSecond = Game(handsSecond);
+        That(gameWinnerSecond, Is.EqualTo(Player.Second));
+        
+        Dictionary<Player, List<Card>> handsNone = new Dictionary<Player, List<Card>>
+        {
+            [Player.First] = new List<Card> {queen1},
+            [Player.Second] = new List<Card> {queen2}
+        };
+        var gameWinnerNone = Game(handsNone);
+        That(gameWinnerNone, Is.EqualTo(null));
     }
 }
